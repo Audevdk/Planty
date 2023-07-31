586c9ddfb649ec7a29c187492e4cc76df7bca0b6
aude vdk <aude.vdk1402@gmail.com>
2023-07-31 09:51:07 +0200

    hook

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -2,8 +2,19 @@
 /**
 ** activation theme
 **/
+
+
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
 
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+   
+    if (is_user_logged_in() && $args->menu == 'header') {
+        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a class="hfe-menu-item" href="http://localhost/wp-admin/">Admin</a></li>';
+ }
+return $items;
 }
